856d58fa1788df573da81a2f9437c38a2b83ff22
Kaique <kaiquesimoes58@gmail.com>
2024-10-31 17:02:12 -0300

    Ajustes para o deploy

--- api/views.py
+++ api/views.py
@@ -13,26 +13,20 @@ class MovieView(APIView):
         if not search_title:
             return Response({"error": "O parâmetro 'Title' é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
         
-        movies = Movie.objects.filter(title__icontains=search_title)
-        if movies.exists():
-            page = int(request.query_params.get('page', 1))
-            items_per_page = 5
-            total_items = movies.count()
-            total_pages = ceil(total_items / items_per_page)
-            start_index = (page - 1) * items_per_page
-            end_index = start_index + items_per_page
+        page = int(request.query_params.get('page', 1))
+        items_por_page = 5
 
-            paginated_movies = movies[start_index:end_index]
-            serializer = MoviesSerializer(paginated_movies, many=True)
-            return Response({
-                "total_pages": total_pages,
-                "current_page": page,
-                "results": serializer.data
-            }, status=status.HTTP_200_OK)
+        movies = Movie.objects.filter(title__icontains=search_title)
+        total_items = movies.count()
+        total_pages = ceil(total_items / items_por_page)
+        start_index = (page - 1) * items_por_page
+        end_index = start_index + items_por_page
+        paginated_movies = movies[start_index:end_index]
         
+        serializer = MoviesSerializer(paginated_movies, many=True)
+
         key = '5e7b999c'
         url = f"http://www.omdbapi.com/?apikey={key}"
-
         params = {
             's': search_title,
             'type': 'movie',
@@ -40,11 +34,11 @@ class MovieView(APIView):
             'page': 1
         }
         
-        movies_data = []
         try:
             response = requests.get(url, params=params)
             data = response.json()
-            
+            movies_data = []
+
             if data.get('Response') == 'True':
                 total_results = int(data.get('totalResults', 0))
                 external_total_pages = ceil(total_results / 10)
@@ -56,32 +50,27 @@ class MovieView(APIView):
                     
                     if data.get('Response') == 'True':
                         for movie_data in data['Search']:
-                            movie, created = Movie.objects.get_or_create(
-                                title=movie_data['Title'],
-                                year=int(movie_data['Year']),
+                            movie, created = Movie.objects.update_or_create(
                                 imdbID=movie_data['imdbID'],
-                                typeMovie=movie_data['Type'],
-                                poster=movie_data['Poster']
+                                defaults={
+                                    'title': movie_data['Title'],
+                                    'year': int(movie_data['Year']),
+                                    'typeMovie': movie_data['Type'],
+                                    'poster': movie_data['Poster']
+                                }
                             )
                             movies_data.append(movie)
                     else:
                         break
                 
-                page = int(request.query_params.get('page', 1))
-                items_per_page = 5
-                total_pages = ceil(len(movies_data) / items_per_page)
-                start_index = (page - 1) * items_per_page
-                end_index = start_index + items_per_page
+                total_items = Movie.objects.filter(title__icontains=search_title).count()
+                total_pages = ceil(total_items / items_por_page)
 
-                paginated_movies = movies_data[start_index:end_index]
-                serializer = MoviesSerializer(paginated_movies, many=True)
-                return Response({
-                    "total_pages": total_pages,
-                    "current_page": page,
-                    "results": serializer.data
-                }, status=status.HTTP_200_OK)
-            else:
-                return Response({"error": data.get("Error")}, status=status.HTTP_404_NOT_FOUND)
-        
+            return Response({
+                "total_pages": total_pages,
+                "current_page": page,
+                "results": serializer.data
+            }, status=status.HTTP_200_OK)
+
         except requests.RequestException:
             return Response({"error": "Falha na conexão com a API externa."}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
